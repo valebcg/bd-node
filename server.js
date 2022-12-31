504ddbd8cd97a85ba9ffbd3a94168c204cc5ad82
valebcg <valeria.cortes.g@gmail.com>
2022-12-31 19:28:31 -0300

    raising the server

--- server.js
+++ server.js
@@ -10,3 +10,6 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'))
+
+// ----levantando el servidor----
+app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"))
