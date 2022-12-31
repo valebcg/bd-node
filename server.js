1bc1938ff9aff7cb2e494945a76228c187f51700
valebcg <valeria.cortes.g@gmail.com>
2022-12-31 19:31:02 -0300

    fetching posts, adding posts, adding condition

--- server.js
+++ server.js
@@ -13,3 +13,46 @@ app.use(express.static('public'))
 
 // ----levantando el servidor----
 app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"))
+
+// ----Traer html para la vista del navegador---
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+
+// ----consigue los posts----
+app.get("/posts", async (req, res) => {
+    const posts = await getPosts()
+    res.json(posts)
+})
+
+// ----agregando un posts----
+app.post("/posts", async (req, res) => {
+    try {
+        const payload = req.body
+        const resultDuplicate = await duplicatePost(payload)
+        if (
+            payload.id === "" ||
+            payload.titulo === "" ||
+            payload.url === "" ||
+            payload.descripcion === ""
+            
+
+        ) {
+            res.send("Faltan datos por ingresar");
+        } else if (resultDuplicate[0].num > 0){
+            res.send("Registro existente")
+            
+           
+        } else{
+            await addPosts(payload) 
+            res.send("Registro agregado con éxito")
+            
+        }
+
+    } catch (err) {
+        res.json({message: "Faltan campos por ingresar"});
+    }  
+    });
+
+
